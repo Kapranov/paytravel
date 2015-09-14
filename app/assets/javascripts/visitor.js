bc441ddf60171f7f11e7a4cac4d162acd739c32e
Kapranov <lugatex@yahoo.com>
2015-09-14 15:33:03 +0300

    added clock js

--- app/assets/javascripts/visitor.js
+++ app/assets/javascripts/visitor.js
@@ -1,3 +1,19 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap.min.js
+
+$(document).ready(function() {
+  var currentdate = new Date();
+	var l=0;
+	function callme()
+	{
+		currentdate = new Date();
+		$('.hour').html(currentdate.getHours());
+		$('.min').html(currentdate.getMinutes());
+		$('.second').html(currentdate.getSeconds());
+		$('.data').css({'padding-left':l+'px'});
+		l=l+5;
+	}
+window.setInterval(callme,1000);
+
+});
